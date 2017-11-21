ba86cc9d8d369e9bb77f1ff728e6dda33813510a
RamPageMz <1033516561@qq.com>
2017-11-21 16:25:53 +0800

    '完成登录验证'

--- grails-app/controllers/grails/second/AdminUserController.groovy
+++ grails-app/controllers/grails/second/AdminUserController.groovy
@@ -1,5 +1,7 @@
 package grails.second
 
+import grails.converters.JSON
+
 class AdminUserController {
     static namespace = "login"
 
@@ -8,24 +10,70 @@ class AdminUserController {
     def index() {
         def one = AdminUser.first()
 
-        [firstName:one.ad_username]
+        [firstName: one.ad_username]
     }
 
-    def addNew(){
+    /**
+     *  Method Name : login
+     *  Description : 登录处理
+     *  Parameter   : username password
+     *  Return      : backCode 是否成功  BackMessage 返回的信息
+     *  Date        : 2017/11/20 17:08
+     */
+    def login() {
+        //todo 密码加密
+        println("login params:" + params.username + " " + params.password)
+
+        def backCode        // 是否成功登录
+        def backMessage     // 返回的信息
+
+        if (params.username == "" || params.username == null || params.password == "" || params.password == null) {
+            backCode=0
+            backMessage="用户名密码为空！"
+        }else {
+            def user=AdminUser.findByAd_username(params.username)
+
+            if (user == null){
+                backCode=0
+                backMessage="用户不存在！"
+            }else if (user.ad_password==params.password){
+                backCode=1
+                backMessage="登录成功"
+            }else {
+                backCode=0
+                backMessage="密码错误！"
+            }
+        }
+
+        // 返回处理
+//        render(contentType: "application/json") {
+//            def result = ["backCode":backCode,"backMessage":backMessage]
+//            render result as JSON
+//        }
+
+        def result=["backCode":backCode,"backMessage":backMessage]
+
+        render result as JSON
+
+
+
+    }
+
+    def addNew() {
         def one = new AdminUser()
 
-        one.ad_username= "mazhen619"
-        one.ad_password="10335a"
-        one.ad_number="007"
-        one.ad_status=1
+        one.ad_username = "mazhen619"
+        one.ad_password = "10335a"
+        one.ad_number = "007"
+        one.ad_status = 1
 
         adminUserService.save(one)
 
     }
 
-    def editUser(){
+    def editUser() {
         def one = AdminUser.last()
-        one.ad_password="1033516561"
+        one.ad_password = "1033516561"
 
         adminUserService.save(one)
     }
--- grails-app/views/login/adminUser/index.gsp
+++ grails-app/views/login/adminUser/index.gsp
@@ -1,1 +1,28 @@
 --${firstName}
+
+<script src="http://libs.baidu.com/jquery/1.7.2/jquery.min.js"></script>
+
+<br/>
+<input type="text" id="login_username"/>
+<br/>
+<input type="password" id="login_password"/>
+<br/>
+<button id="loginBtn" onclick="login();">登录</button>
+
+<script>
+
+    function login() {
+
+        console.log("aaaa");
+
+        var name=$("#login_username").val();
+        var pwd=$("#login_password").val();
+
+        $.post("/adminUser/login", {username: name, password: pwd}, function (dData) {
+            console.log(dData.backCode);
+            console.log(dData.backMessage);
+        }, "json");
+
+    }
+
+</script>
